fdc346d2bf390bf554e9cbc62eea85d7f755cc99
Charles Alford <cm205@Charless-MacBook-Pro.local>
2020-03-30 14:50:32 -0500

    Coronavirus

--- HereWeGo/HereWeGo/Program.cs
+++ HereWeGo/HereWeGo/Program.cs
@@ -7,6 +7,7 @@ namespace HereWeGo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Coronavirus is a bummer!");
         }
     }
 }
